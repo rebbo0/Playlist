72764cfb8ee78e144228ac8c297aaa15651b994b
rebbo0 <robin.tippelreiter@sz-ybbs.ac.at>
2023-10-16 15:34:18 +0200

    css

--- playlist.js
+++ playlist.js
@@ -3,8 +3,12 @@ var url = "playlist.json";
 
 xmlhttp.onreadystatechange = function () {
     var jsonData = JSON.parse(this.responseText);
-    displayPlaylist(jsonData.cards);    
+    displayPlaylist(jsonData.lists);    
 };
 
 xmlhttp.open("GET", url);
 xmlhttp.send();
+
+function displayPlaylist(lists){
+    
+}
