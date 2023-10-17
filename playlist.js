75bb7382d9e02f96479cf52e6e5f9c1f96b116d4
rebbo0 <robin.tippelreiter@sz-ybbs.ac.at>
2023-10-17 08:53:49 +0200

    janko_stund

--- playlist.js
+++ playlist.js
@@ -3,12 +3,28 @@ var url = "playlist.json";
 
 xmlhttp.onreadystatechange = function () {
     var jsonData = JSON.parse(this.responseText);
-    displayPlaylist(jsonData.lists);    
+    displayPlaylist(jsonData.playlists);    
 };
 
 xmlhttp.open("GET", url);
 xmlhttp.send();
 
-function displayPlaylist(lists){
+function displayPlaylist(playlists){
+    var maindiv = document.getElementById('maindiv');
+    maindiv.className = "maindiv";
 
+    playlists.forEach(function (playlist)
+    {
+        var listdiv = document.createElement('div');
+        listdiv.className="listdiv";
+
+        var listname = document.createElement('h2');
+        listname.className="listname";
+        var listowner = document.createElement('h3');
+        listowner.className = "listowner";
+        var listtitles = document.createElement
+
+
+        maindiv.appendChild("listdiv");
+    });
 }
