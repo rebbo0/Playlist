1413bf6c30de5b46129b86e3b8b21c3a11494c6c
Davidoida <david.muthentaler@sz-ybbs.ac.at>
2023-10-18 10:31:03 +0200

    indy - foreach song

--- playlist.js
+++ playlist.js
@@ -2,11 +2,9 @@ var xmlhttp = new XMLHttpRequest();
 var url = "playlist.json";
 
 xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var jsonData = JSON.parse(this.responseText);
-    displayPlaylist(jsonData);
-  }
-}
+  var jsonData = JSON.parse(this.responseText);
+  displayPlaylist(jsonData);
+};
 
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
@@ -19,32 +17,34 @@ function displayPlaylist(playlists) {
   playlists.forEach((playlist) => {
     listdiv = document.createElement("div");
     listdiv.className = "listdiv";
-    
-    
-    var listname = document.createElement('h2');
-    listname.className="listname";
+
+    var listname = document.createElement("h2");
+    listname.className = "listname";
     listname.textContent = playlist.name;
     listdiv.appendChild(listname);
-    var listowner = document.createElement('h3');
+    var listowner = document.createElement("h3");
     listowner.className = "listowner";
-    
-    var listtitles = document.createElement('div');
+
+    var listtitles = document.createElement("div");
     listtitles.className = "listtitles";
-        
-        playlist.songs.forEach((song) => {
-            var songtitle = document.createElement('p');
-            songtitle.className = "songtitle";
-            var songinterpret = document.createElement('p');
-            songinterpret.className = "songinterpret";
-            var songlength = document.createElement('p');
-            songlength.className = "songlength";
 
-        });        
-        
-        maindiv.appendChild(listdiv);
+    playlist.songs.forEach((song) => {
+      var songtitle = document.createElement("p");
+      songtitle.className = "songtitle";
+      songtitle.textContent = song.title;
+      listtitles.appendChild(songtitle);
 
+      var songinterpret = document.createElement("p");
+      songinterpret.className = "songinterpret";
+      songtitle.textContent = song.interpret;
+      listtitles.appendChild(songinterpret);
+
+      var songlength = document.createElement("p");
+      songlength.className = "songlength";
+      songlength.textContent = song.length;
+      listtitles.appendChild(songlength);
     });
 
-
+    maindiv.appendChild(listdiv);
+  });
 }
-
