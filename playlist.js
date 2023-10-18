f22d3421efb78d70e7db457e0dfcd9c92d951157
Davidoida <david.muthentaler@sz-ybbs.ac.at>
2023-10-18 09:29:16 +0200

    indy1

--- playlist.js
+++ playlist.js
@@ -2,29 +2,28 @@ var xmlhttp = new XMLHttpRequest();
 var url = "playlist.json";
 
 xmlhttp.onreadystatechange = function () {
-    var jsonData = JSON.parse(this.responseText);
-    displayPlaylist(jsonData.playlists);    
+  var jsonData = JSON.parse(this.responseText);
+  displayPlaylist(jsonData.playlists);
 };
 
 xmlhttp.open("GET", url);
 xmlhttp.send();
 
-function displayPlaylist(playlists){
-    var maindiv = document.getElementById('maindiv');
-    maindiv.className = "maindiv";
+function displayPlaylist(playlists) {
+  var maindiv = document.getElementById("maindiv");
+  maindiv.className = "maindiv";
 
-    playlists.forEach(function (playlist)
-    {
-        var listdiv = document.createElement('div');
-        listdiv.className="listdiv";
+  playlists.forEach(function (playlist) {
+    var listdiv = document.createElement("div");
+    listdiv.className = "listdiv";
 
-        var listname = document.createElement('h2');
-        listname.className="listname";
-        var listowner = document.createElement('h3');
-        listowner.className = "listowner";
-        var listtitles = document.createElement
+    var listname = document.createElement("h2");
+    listname.className = "listname";
+    var listowner = document.createElement("h3");
+    listowner.className = "listowner";
+    var listtitles = document.createElement;
 
-
-        maindiv.appendChild("listdiv");
-    });
+    maindiv.appendChild("listdiv");
+  });
 }
+//test
